58fdd8ad9e52be07cd6f1e57cc85d99fb2628603
Melanie-J-Baker <baker_mel@hotmail.com>
2024-05-14 11:03:13 +0100

    Remove public folder

--- routes/characters.js
+++ routes/characters.js
@@ -1,12 +1,11 @@
 const express = require("express");
 const router = express.Router();
-//const authAdmin = require("../middleware/authAdmin");
 
 const char_controller = require("../controllers/char_controller");
 
 router.get("/", char_controller.get_chars);
 router.post("/:id", char_controller.get_char_coords);
-router.post("/", /*authAdmin,*/ char_controller.post_chars);
-router.delete("/", /*authAdmin,*/ char_controller.delete_chars);
+router.post("/", char_controller.post_chars);
+router.delete("/", char_controller.delete_chars);
 
 module.exports = router;
--- routes/game.js
+++ routes/game.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const router = express.Router();
-//const authAdmin = require("../middleware/authAdmin");
 
 const game_controller = require("../controllers/game_controller");
 
@@ -11,6 +10,6 @@ router.post("/", game_controller.post_game);
 router.patch("/:id/time", game_controller.patch_time);
 router.patch("/:id/name", game_controller.patch_name);
 
-router.delete("/", /*authAdmin,*/ game_controller.delete_games);
+router.delete("/", game_controller.delete_games);
 
 module.exports = router;
